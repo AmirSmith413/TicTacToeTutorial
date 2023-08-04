e60b28fb986120cb630ea9eabf4216e818cb5138
AmirSmith413 <amirsmith166@gmail.com>
2023-08-03 20:03:01 -0700

    moved some things around

--- TicTacToeTutorial/src/components/Board.jsx
+++ TicTacToeTutorial/src/components/Board.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 
-function Square({value}) {
-  const [value, setValue] = useState(null)
-  function handleClick(){
-    setValue('X')
-  }
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({value,onSquareClick}) {
+ 
+  
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
-  const [squares, setsquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
   <>
     <div className="board-row">
