1d9f1a4b1c664d742dd88a824622fcd12c7f3073
AmirSmith413 <amirsmith166@gmail.com>
2023-08-03 19:40:37 -0700

    starting from scratch

--- TicTacToeTutorial/src/App.jsx
+++ TicTacToeTutorial/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Board from "./components/Board";
-import NextBoard from './components/NextBoard'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +9,7 @@ function App() {
   return (
     <>
     <Board/>
-    <NextBoard/>
+    
     </>
    
   );
